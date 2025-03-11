6ed939f3b27a07aa0b85a71485e629f8d1d54e79
thergh <lukasz.ciswicki@gmail.com>
2025-03-11 16:18:23 +0100

    JUnit init

---
+++ src/test/java/org/example/TransactionTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionTest {
+
+    @Test
+    void getAmount() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
